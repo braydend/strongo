a2d8dc43f39e137b5d9a73644a61070c97aa628c
Brayden <braydendiluca@gmail.com>
2020-11-29 12:06:17 +1100

    Add CORS middleware

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 // startServer - Boot the API server and listen on port 8080
 func startServer(db *gorm.DB) {
 	r := gin.Default()
+	router.AllowCORS(r, []string{"*"})
 	router.LoadRouter(r, db)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- utils/router/router.go
+++ utils/router/router.go
@@ -3,10 +3,24 @@ package router
 import (
 	"net/http"
 	"strongo/controllers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
+
+// AllowCORS - Allow CORS from the allowed origins passed as an argument
+func AllowCORS(router *gin.Engine, allowedOrigins []string) {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     allowedOrigins,
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
 
 // LoadRouter - Configure the routes for the webserver
 func LoadRouter(router *gin.Engine, db *gorm.DB) {
